7c20bec19b38414763f4049c1ce84a6bf045e5fe
kimcc <15278020+kimcc@users.noreply.github.com>
2021-10-27 10:00:03 -0500

    file setup

---
+++ models/Thought.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+
+  }
+);
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
